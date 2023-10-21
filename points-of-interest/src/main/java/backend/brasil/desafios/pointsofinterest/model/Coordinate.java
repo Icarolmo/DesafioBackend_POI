26e0bff32665e20c024e10cacb6f65efc38adf71
Icarus <icarolmol@gmail.com>
2023-10-21 19:30:24 -0300

    Fix: create Coordinate class;

---
+++ points-of-interest/src/main/java/backend/brasil/desafios/pointsofinterest/model/Coordinate.java
@@ -0,0 +1,19 @@
+package backend.brasil.desafios.pointsofinterest.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Coordinate {
+
+    long coordinate_x;
+
+    long coordinate_y;
+
+
+}
