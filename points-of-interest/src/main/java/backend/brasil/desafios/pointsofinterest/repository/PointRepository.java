a8d0bdf4be236658d2fffb8574a82a763abbec62
Icarus <icarolmol@gmail.com>
2023-10-21 21:51:48 -0300

    Style: refactor paths

--- points-of-interest/src/main/java/backend/brasil/desafios/pointsofinterest/repository/PointRepository.java
+++
@@ -1,7 +0,0 @@
-package backend.brasil.desafios.pointsofinterest.repository;
-
-import backend.brasil.desafios.pointsofinterest.model.Point;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PointRepository extends JpaRepository<Point, String> {
-}
--- points-of-interest/src/main/resources/db/migration/V1__create_table_poi.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE point_of_interest (
-    id SERIAL PRIMARY KEY UNIQUE NOT NULL,
-    name_poi VARCHAR(50) NOT NULL,
-    coordinate_x BIGINT NOT NULL,
-    coordinate_y BIGINT NOT NULL
-);
